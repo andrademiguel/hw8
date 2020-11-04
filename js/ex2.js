994dccd23089f2160a6d4fdfd691501f2699e9f6
andrademiguel <miguel4andrade@gmail.com>
2020-11-03 16:17:55 -0800

    WIP 3

--- js/ex2.js
+++ js/ex2.js
@@ -14,16 +14,10 @@ forms.addEventListener("submit", e => {
       const nameElement = document.createElement("div");
       nameElement.textContent = user.name;
       nameElement.style.fontSize = "15px";
-      const websiteElement = document.createElement("a");
-      websiteElement.href = user.blog;
-      websiteElement.textContent = websiteElement.href;
       const infosElement = document.getElementById("infos");
-      infosElement.innerHTML = ""; // Remove previous user info
+      infosElement.innerHTML = ""; 
       infosElement.appendChild(pictureElement);
       infosElement.appendChild(nameElement);
-      infosElement.appendChild(websiteElement);
     })
-    .catch(err => {
-      console.error(err.message);
-    });
+
 });
